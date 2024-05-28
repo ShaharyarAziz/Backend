d5233db911e07547168655518854f2cf9a5badb7
Shaharyar Aziz <80951726+ShaharyarAziz@users.noreply.github.com>
2024-05-28 14:30:00 +0500

    New Changes

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/twitter", (req, res) => {
+  res.send("twitter");
+});
+app.get("/Login", (req, res) => {
+  res.send("<h2>You are at the login route</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
